0dccc431f4c66083bacc0f1f487a88137f5b68e1
momodayoo <msyauqi94@gmail.com>
2024-04-07 23:14:46 +0800

    feature/quiz game

---
+++ quiz_game.py
@@ -0,0 +1,51 @@
+print ("Welcome to the quiz game!")
+
+playing = input ("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print ("Okay! Let's play :)")
+score = 0
+
+answer = input ("What does CPU stand for? ")
+if answer == "central processing unit":
+    print ("Correct!")
+    score += 1   
+else:
+    print ("Incorrect!")
+
+answer = input ("What does GPU stand for? ")
+
+if answer == "graphics processing unit":
+    print ("Correct!")
+    score += 1
+else:
+    print ("Incorrect!")
+
+answer = input ("What does RAM stand for? ")
+
+if answer == "random access memory":
+    print ("Correct!")
+    score += 1
+else:
+    print ("Incorrect!")
+
+answer = input ("What does PSU stand for? ")
+
+if answer == "power supply unit":
+    print ("Correct!")
+    score += 1
+else:
+    print ("Incorrect!")
+
+print ("You got " + str(score) + " questions correct!")
+
+print ("You got " + str((score/4) * 100) + "%.")
+
+print ("Thanks for playing!")
+
+
+
+
+# Path: quiz_game.py
